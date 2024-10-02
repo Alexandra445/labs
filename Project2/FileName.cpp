994305c7c29ff73ed385752b31e0525408fae23b
Alexandra445 <galeevalexandra@gmail.com>
2024-10-02 15:10:53 +0300

    Update FileName.cpp

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -1,15 +1,13 @@
-#include<iostream> 
-#include<cmath> 
-#include<iomanip> 
+#include <cmath> 
 #include <cstring>
-
-using namespace std;
+#include <iomanip> 
+#include <iostream>  
+#include <limits>
 
 int n, x, f, mx, mn, k1, k2, k3, p1, p2, p3;
-double sum, y;
+double sum;
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
     setlocale(LC_ALL, "RUS");
 
     bool isHuman = true;
@@ -19,49 +17,71 @@ int main(int argc, const char* argv[])
     }
 
     if (isHuman) {
-        cout << "Введите количество чисел в последовательности: " << endl;
+        std::cout << "Введите количество чисел в последовательности: " << std::endl;
     }
-    cin >> n;
-    mn = 1000000;
+
+    std::cin >> n;
+    // используем предельное значение типа данных int
+    mn = std::numeric_limits<int>::max();
+    mx = std::numeric_limits<int>::min();
 
     if (isHuman) {
-        cout << "Введите последовательность положительных целых чисел: " << endl;
+        std::cout << "Введите последовательность положительных целых чисел: " << std::endl;
     }
 
-    for (int i = 0; i < n; i++) {
-        cin >> x;
+    for (size_t i = 0; i < n; ++i) {
 
-        // Находим простые числа последовательности 
+        std::cin >> x;
+
         f = 0;
-        if (x != 1) {
-            for (int j = 2; j <= sqrt(x); j++) {
+        if (x == 2) {
+            if (isHuman) {
+                std::cout << x << " является простым числом." << std::endl;
+            }
+            else {
+                std::cout << x << std::endl;
+            }
+        }
+        else if (x > 2 && x % 2 != 0) {  // пропускаем четные числа, кроме 2
+            int sqrt_x = std::sqrt(x);  // вычисляем корень один раз
+            for (int j = 3; j <= sqrt_x; j += 2) {  // проверяем только нечётные числа
                 if (x % j == 0) {
                     f = 1;
+                    break;  // прекращаем проверку при нахождении делителя
                 }
             }
             if (f == 0) {
-                cout << x << endl;
+                if (isHuman) {
+                    std::cout << x << " является простым числом." << std::endl;
+                }
+                else {
+                    std::cout << x << std::endl;
+                }
             }
         }
 
-        // Находим сумму всех элементов последовательности 
+        // Суммирование всех чисел
         sum += x;
-        // Находим min и max в последовательности 
-        mn = min(mn, x);
-        mx = max(mx, x);
-        // Находим количество чисел кратных 5 
+
+        // Поиск минимума и максимума
+        mn = std::min(mn, x);
+        mx = std::max(mx, x);
+
+        // Количество чисел, кратных 5
         if (x % 5 == 0) {
             k1++;
         }
-        // Находим количество чисел являющихся степенью двойки 
-        y = x;
+
+        // Проверка, является ли число степенью двойки
+        double y = x;
         while (y > 1) {
             y /= 2;
         }
         if (y == 1) {
             k2++;
         }
-        // Находим количество чисел, превышающих сумму двух предшествующих членов последовательности 
+
+        // Количество чисел, превышающих сумму двух предыдущих
         p1 = p2;
         p2 = p3;
         p3 = x;
@@ -71,21 +91,21 @@ int main(int argc, const char* argv[])
             }
         }
     }
-    if (isHuman)
-    {
-        cout << "Среднее арифметическое введеных чисел: " << fixed << setprecision(2) << (sum) / n << endl;
-        cout << "Разница между максимумом и минимумом: " << mx - mn << endl;
-        cout << "Количество чисел, кратных числу 5: " << k1 << endl;
-        cout << "Количество чисел, являющихся степенью двойки: " << k2 << endl;
-        cout << "Количество чисел, превышающих сумму двух предшествующих членов последовательности: " << k3 << endl;
+
+    if (isHuman) {
+        std::cout << "Среднее арифметическое введеных чисел: " << std::fixed << std::setprecision(2) << (sum) / n << std::endl
+            << "Разница между максимумом и минимумом: " << mx - mn << std::endl
+            << "Количество чисел, кратных числу 5: " << k1 << std::endl
+            << "Количество чисел, являющихся степенью двойки: " << k2 << std::endl
+            << "Количество чисел, превышающих сумму двух предшествующих членов последовательности: " << k3 << std::endl;
     }
-    else
-    {
-     cout << fixed << setprecision(2) << (sum) / n << endl; 
-     cout << mx - mn << endl;
-     cout << k1 << endl;
-     cout << k2 << endl;
-     cout << k3 << endl;
+    else {
+        std::cout << std::fixed << std::setprecision(2) << (sum) / n << std::endl
+            << mx - mn << std::endl
+            << k1 << std::endl
+            << k2 << std::endl
+            << k3 << std::endl;
     }
+
     return 0;
 }
